795974aa8af16ce12aa23824b4796a073ffce9cd
Adam Tahoun <ataho001@fiu.edu>
2017-09-30 18:40:17 -0400

    created home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1 className="App-title"> LIKE A GIRL </h1>
+        <hr/>
+        <h1 className="App-about"> ABOUT US </h1>
+        <h1 className="App-anonymous"> ANONYMOUS ASKING </h1>
+        <h1 className="App-culture"> FEMALE CULTURE </h1>
+        <h1 className="App-gallery"> GIRLS GALLERY </h1>
       </div>
     );
   }
