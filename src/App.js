900d6a1c38bfa7c9c2fd5c7c73dd753498968f14
Adam Tahoun <ataho001@fiu.edu>
2017-10-01 02:17:39 -0400

    added material desgin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 class About extends Component {
   render() {
@@ -25,6 +25,17 @@ class About extends Component {
 }
 
 class Ask extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      askQuestion: '',
+      listOfQuestions: [
+        'How do you shave your legs..? Asking for a friend',
+        'WHAT DO YOU DO WHEN YOU GET YOUR PERIOD!!!???'
+      ]
+
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -35,8 +46,55 @@ class Ask extends Component {
         <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
         <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
       </header>
+      <Card>
+      <CardHeader
+        title="URL Avatar"
+        subtitle="Subtitle"
+        avatar="images/jsa-128.jpg"
+      />
+      <form onSubmit={this.addTask}>
+        <input type="text"
+          value={this.state.askQuestion}
+          onChange={this.handleTaskTyping} />
+        <button> Ask Question</button>
+      </form>
+      </Card>
+
+      <ul>
+        {this.renderTaskItems()}
+      </ul>
       </div>
     )
+  }
+
+  addTask = (event) => {
+      event.preventDefault();
+      this.setState(function (previousState){
+        previousState.listOfQuestions.push(previousState.askQuestion);
+        return{
+          listOfTask: previousState.listOfTask,
+          askQuestion: ''
+        };
+      });
+  }
+
+  renderTaskItems(){
+    var  taskItemHtml =
+      this.state.listOfQuestions.map((task, index) => {
+        return (
+          <li>
+            {task}
+          </li>
+        );
+      });
+      return taskItemHtml;
+  }
+
+  handleTaskTyping = (event) => {
+    this.setState({
+      askQuestion: event.target.value
+  });
+
   }
 }
 
@@ -84,13 +142,12 @@ class Home extends Component {
       </div>
     )
   }
-
 }
-
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <Router>
         <div>
           <Route exact path="/" component={Home}/>
@@ -100,6 +157,7 @@ class App extends Component {
           <Route path="/gallery" component={Gallery}/>
         </div>
       </Router>
+      </MuiThemeProvider>
     );
   }
 }
