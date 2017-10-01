73ef8e7b54cf7035787590095498b9b7b804ff99
Adam Tahoun <ataho001@fiu.edu>
2017-10-01 12:27:33 -0400

    finished the QA system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import user from './user.jpg'
 import {
   BrowserRouter as Router,
   Route,
@@ -7,6 +8,11 @@ import {
 } from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import {List, ListItem} from 'material-ui/List';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 class About extends Component {
   render() {
@@ -32,8 +38,11 @@ class Ask extends Component {
       listOfQuestions: [
         'How do you shave your legs..? Asking for a friend',
         'WHAT DO YOU DO WHEN YOU GET YOUR PERIOD!!!???'
+      ],
+      qas: [
+        ["When shaving your ledgs it's important to have a new clean razor and shaving cream(conditionar works juat ask well. Once you've put your cream on your legs you're going to take your razor and statring at the ankle move in an upwards motions. )"],
+        []
       ]
-
     };
   }
   render() {
@@ -46,46 +55,54 @@ class Ask extends Component {
         <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
         <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
       </header>
-      <Card>
+      <Card style={{"margin": "10px", "width": "700px", "margin-left": "500px", "padding-bottom": "10px"}}>
       <CardHeader
-        title="URL Avatar"
-        subtitle="Subtitle"
-        avatar="images/jsa-128.jpg"
+        title="Samantha Perez"
+        avatar={user}
       />
       <form onSubmit={this.addTask}>
-        <input type="text"
-          value={this.state.askQuestion}
-          onChange={this.handleTaskTyping} />
-        <button> Ask Question</button>
+      <TextField
+        hintText="Ask a question"
+        value={this.state.askQuestion}
+        onChange={this.handleTaskTyping}
+      />
+     <br/>
+     <FlatButton label="Submit" onClick={this.addTask} secondary={true}/>
       </form>
       </Card>
 
-      <ul>
         {this.renderTaskItems()}
-      </ul>
+
       </div>
     )
   }
 
   addTask = (event) => {
-      event.preventDefault();
-      this.setState(function (previousState){
-        previousState.listOfQuestions.push(previousState.askQuestion);
-        return{
-          listOfTask: previousState.listOfTask,
-          askQuestion: ''
+      this.setState(function (previousState) {
+        let newQuestions = previousState.listOfQuestions.concat([this.state.askQuestion]);
+        let nqas = previousState.qas.slice();
+        nqas.push([]);
+
+        return {
+          listOfQuestions: newQuestions,
+          qas: nqas
         };
       });
   }
 
+  addAnswer = (index, answer) => {
+    let taskAnswers = this.state.qas[index];
+    var newanswers = taskAnswers.concat([answer]);
+    taskAnswers[index] = newanswers;
+    this.setState({
+      qas: taskAnswers
+    });
+  }
+
   renderTaskItems(){
-    var  taskItemHtml =
-      this.state.listOfQuestions.map((task, index) => {
-        return (
-          <li>
-            {task}
-          </li>
-        );
+    var taskItemHtml =
+      this.state.listOfQuestions.map((question, index) => {
+        return (<QuestionAnswer key={index} task={question} answers={this.state.qas[index]} onAdd={answer => this.addAnswer(index, answer)}/>);
       });
       return taskItemHtml;
   }
@@ -93,8 +110,55 @@ class Ask extends Component {
   handleTaskTyping = (event) => {
     this.setState({
       askQuestion: event.target.value
-  });
+    });
+  }
+}
 
+class QuestionAnswer extends React.Component {
+  state = {
+    answer: ""
+  }
+
+  handleAnswerTyping = (event) => {
+    this.setState({
+      answer: event.target.value
+    });
+  }
+
+  onAddClick = () => {
+    this.props.onAdd(this.state.answer);
+    this.setState({
+      answer: ""
+    })
+  }
+
+  render() {
+    console.log(this.props.answers);
+
+    return (
+      <div>
+        <Card style={{"margin": "10px", "width": "700px", "margin-left": "500px", "padding-bottom": "10px"}}>
+          <List>
+            <ListItem>
+            <h2>
+            {this.props.task}
+            </h2>
+            </ListItem>
+
+            <div>
+            {this.props.answers && this.props.answers.map(ans => <li key={ans} style={{"list-style":"none"}}>{ans}</li>)}
+
+            </div>
+            <TextField
+              placeholder="Answer Question"
+              value={this.state.answer}
+              onChange={this.handleAnswerTyping}
+            />
+            <RaisedButton label="Answer" secondary={true} onClick={this.onAddClick} style={{"margin-left": "100px"}} />
+          </List>
+        </Card>
+      </div>
+    );
   }
 }
 
@@ -109,6 +173,7 @@ class Culture extends Component {
         <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
         <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
       </header>
+      <h1> Cultre Page Coming Soon...</h1>
       </div>
     )
   }
@@ -125,6 +190,7 @@ class Gallery extends Component {
         <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
         <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
       </header>
+          <h1> Gallery Page Coming Soon...</h1>
       </div>
     )
   }
