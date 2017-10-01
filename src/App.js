063a82a5e6c027effaa0fefac7d099abf6c6f61d
Adam Tahoun <ataho001@fiu.edu>
2017-10-01 00:36:45 -0400

    finished home page and header bar for all pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,105 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
+class About extends Component {
   render() {
     return (
       <div className="App">
+      <header className="App-header">
+        <h1> <Link className="App-title-bar" to="/"> LIKE A GIRL </Link> </h1>
+        <h1> <Link className="App-about-bar" to="/about"> ABOUT US </Link> </h1>
+        <h1> <Link className="App-ask-bar" to="/ask"> ANONYMOUS ASKING </Link> </h1>
+        <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
+        <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
+      </header>
+      </div>
+    )
+  }
+}
+
+class Ask extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <h1> <Link className="App-title-bar" to="/"> LIKE A GIRL </Link> </h1>
+        <h1> <Link className="App-about-bar" to="/about"> ABOUT US </Link> </h1>
+        <h1> <Link className="App-ask-bar" to="/ask"> ANONYMOUS ASKING </Link> </h1>
+        <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
+        <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
+      </header>
+      </div>
+    )
+  }
+}
+
+class Culture extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <h1> <Link className="App-title-bar" to="/"> LIKE A GIRL </Link> </h1>
+        <h1> <Link className="App-about-bar" to="/about"> ABOUT US </Link> </h1>
+        <h1> <Link className="App-ask-bar" to="/ask"> ANONYMOUS ASKING </Link> </h1>
+        <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
+        <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
+      </header>
+      </div>
+    )
+  }
+}
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <h1> <Link className="App-title-bar" to="/"> LIKE A GIRL </Link> </h1>
+        <h1> <Link className="App-about-bar" to="/about"> ABOUT US </Link> </h1>
+        <h1> <Link className="App-ask-bar" to="/ask"> ANONYMOUS ASKING </Link> </h1>
+        <h1> <Link className="App-culture-bar" to="/culture"> FEMALE CULTURE </Link>  </h1>
+        <h1> <Link className="App-gallery-bar" to="/culture"> GIRLS GALLERY </Link>  </h1>
+      </header>
+      </div>
+    )
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (<div className="App">
         <h1 className="App-title"> LIKE A GIRL </h1>
         <hr/>
-        <h1 className="App-about"> ABOUT US </h1>
-        <h1 className="App-anonymous"> ANONYMOUS ASKING </h1>
-        <h1 className="App-culture"> FEMALE CULTURE </h1>
-        <h1 className="App-gallery"> GIRLS GALLERY </h1>
+        <h1><Link className="App-about" to="/about">ABOUT US</Link> </h1>
+        <h1><Link className="App-anonymous" to="/ask"> ANONYMOUS ASKING</Link> </h1>
+        <h1><Link className="App-culture" to="/culture"> FEMALE CULTURE</Link> </h1>
+        <h1><Link className="App-gallery" to="/gallery"> GIRLS GALLERY</Link> </h1>
       </div>
+    )
+  }
+
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/ask" component={Ask}/>
+          <Route path="/culture" component={Culture}/>
+          <Route path="/gallery" component={Gallery}/>
+        </div>
+      </Router>
     );
   }
 }
